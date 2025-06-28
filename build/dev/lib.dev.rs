4393ab79937f4b636a48692603aeb8ac7f9826e0
illdub <illia.dubrovin@dreamhost.com>
2025-06-28 16:49:35 +0200

    have working version

--- build/dev/lib.dev.rs
+++ build/dev/lib.dev.rs
@@ -1,12 +1,16 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{ HtmlCanvasElement, WebGl2RenderingContext };
-use glow::{ HasContext, Context, Shader, Program };
+use web_sys::{HtmlCanvasElement, WebGl2RenderingContext};
+use glow::Context;
+
+mod index;
+use index::Program;
 
 #[wasm_bindgen(start)]
 pub fn run() {
     console_error_panic_hook::set_once();
     log("🦀 Rust WASM module loaded successfully!");
+    
     if let Err(e) = render() {
         web_sys::console::error_1(&e);
     }
@@ -18,64 +22,22 @@ fn render() -> Result<(), JsValue> {
         .get_element_by_id("webgl-canvas")
         .ok_or("Canvas not found")?
         .dyn_into::<HtmlCanvasElement>()?;
+    
     let context = canvas
         .get_context("webgl2")?
         .ok_or("No WebGL2 context")?
         .dyn_into::<WebGl2RenderingContext>()?;
+    
     let gl = Context::from_webgl2_context(context);
-
-    unsafe {
-        gl.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
-
-        let vs = compile_shader(&gl, glow::VERTEX_SHADER, include_str!("assets/vertex.glsl"))?;
-        let fs = compile_shader(&gl, glow::FRAGMENT_SHADER, include_str!("assets/fragment.glsl"))?;
-        let program = link_program(&gl, vs, fs)?;
-
-        let vao = gl.create_vertex_array()?;
-        gl.bind_vertex_array(Some(vao));
-        gl.use_program(Some(program));
-        gl.clear_color(0.1, 0.2, 0.3, 1.0);
-        gl.clear(glow::COLOR_BUFFER_BIT);
-        gl.draw_arrays(glow::TRIANGLES, 0, 3);
-        gl.use_program(None);
-        gl.bind_vertex_array(None);
-    }
-
+    
+    let program = Program::new(gl)
+        .map_err(|e| JsValue::from_str(&e))?;
+    
+    program.render()
+        .map_err(|e| JsValue::from_str(&e))?;
+    
     log("🔺 Triangle rendered successfully!");
     Ok(())
-}
-
-fn compile_shader(gl: &Context, kind: u32, src: &str) -> Result<Shader, JsValue> {
-    unsafe {
-        let shader = gl.create_shader(kind)?;
-        gl.shader_source(shader, src);
-        gl.compile_shader(shader);
-        if !gl.get_shader_compile_status(shader) {
-            let log = gl.get_shader_info_log(shader);
-            gl.delete_shader(shader);
-            Err(JsValue::from_str(&format!("Shader error: {log}")))
-        } else {
-            Ok(shader)
-        }
-    }
-}
-
-fn link_program(gl: &Context, vs: Shader, fs: Shader) -> Result<Program, JsValue> {
-    unsafe {
-        let program = gl.create_program()?;
-        gl.attach_shader(program, vs);
-        gl.attach_shader(program, fs);
-        gl.link_program(program);
-        gl.delete_shader(vs);
-        gl.delete_shader(fs);
-        if !gl.get_program_link_status(program) {
-            let log = gl.get_program_info_log(program);
-            gl.delete_program(program);
-            Err(JsValue::from_str(&format!("Link error: {log}")))
-        } else {
-            Ok(program)
-        }
-    }
 }
 
 fn log(msg: &str) {
--- build/linux/main.linux.rs
+++ build/linux/main.linux.rs
@@ -1,38 +1,37 @@
 use glutin::config::ConfigTemplateBuilder;
-use glutin::context::{ContextApi, ContextAttributesBuilder, Version};
+use glutin::context::{ ContextApi, ContextAttributesBuilder, Version };
 use glutin::display::GetGlDisplay;
 use glutin::prelude::*;
-use glutin::surface::{SurfaceAttributesBuilder, WindowSurface};
+use glutin::surface::{ SurfaceAttributesBuilder, WindowSurface };
 use glutin_winit::DisplayBuilder;
 use glow::HasContext;
 use raw_window_handle::HasWindowHandle;
 use std::num::NonZeroU32;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, EventLoop};
-use winit::window::{Window, WindowId};
+use winit::event_loop::{ ActiveEventLoop, EventLoop };
+use winit::window::{ Window, WindowId };
+
+mod index;
+use index::Program;
 
 struct App {
     window: Option<Window>,
     gl_context: Option<glutin::context::PossiblyCurrentContext>,
     gl_surface: Option<glutin::surface::Surface<WindowSurface>>,
-    gl: Option<glow::Context>,
-    shader_program: Option<glow::Program>,
-    vao: Option<glow::VertexArray>,
+    program: Option<Program>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         if self.window.is_none() {
-            let window_attributes = Window::default_attributes()
-                .with_title("Native OpenGL Example");
-            
+            let window_attributes =
+                Window::default_attributes().with_title("Native OpenGL Triangle");
+
             let window = event_loop.create_window(window_attributes).unwrap();
-            
-            // Create display builder
+
             let display_builder = DisplayBuilder::new();
-            
-            // Build the display
+
             let (_, gl_config) = display_builder
                 .build(event_loop, ConfigTemplateBuilder::new(), |mut configs| {
                     configs.next().unwrap()
@@ -41,126 +40,63 @@ impl ApplicationHandler for App {
 
             let raw_display = gl_config.display();
 
-            // Create context
             let context_attributes = ContextAttributesBuilder::new()
                 .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 3))))
                 .build(Some(window.window_handle().unwrap().as_raw()));
 
             let not_current_gl_context = unsafe {
-                raw_display
-                    .create_context(&gl_config, &context_attributes)
-                    .unwrap()
+                raw_display.create_context(&gl_config, &context_attributes).unwrap()
             };
 
-            // Create surface
-            let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
-                window.window_handle().unwrap().as_raw(),
-                NonZeroU32::new(800).unwrap(),
-                NonZeroU32::new(600).unwrap(),
-            );
+            let attrs = SurfaceAttributesBuilder::<WindowSurface>
+                ::new()
+                .build(
+                    window.window_handle().unwrap().as_raw(),
+                    NonZeroU32::new(800).unwrap(),
+                    NonZeroU32::new(600).unwrap()
+                );
 
-            let surface = unsafe {
-                raw_display.create_window_surface(&gl_config, &attrs).unwrap()
-            };
+            let surface = unsafe { raw_display.create_window_surface(&gl_config, &attrs).unwrap() };
 
-            // Make context current
             let gl_context = not_current_gl_context.make_current(&surface).unwrap();
 
-            // Load OpenGL functions
             let gl = unsafe {
                 glow::Context::from_loader_function(|s| {
                     raw_display.get_proc_address(&std::ffi::CString::new(s).unwrap()) as *const _
                 })
             };
 
-            // Setup triangle rendering
-            let (shader_program, vao) = unsafe {
-                // Load shader sources
-                let vertex_shader_source = include_str!("assets/vertex.glsl");
-                let fragment_shader_source = include_str!("assets/fragment.glsl");
+            let program = Program::new(gl).expect("Failed to create graphics program");
 
-                // Create vertex shader
-                let vertex_shader = gl.create_shader(glow::VERTEX_SHADER).unwrap();
-                gl.shader_source(vertex_shader, vertex_shader_source);
-                gl.compile_shader(vertex_shader);
-
-                if !gl.get_shader_compile_status(vertex_shader) {
-                    let error = gl.get_shader_info_log(vertex_shader);
-                    panic!("Vertex shader compilation failed: {}", error);
-                }
-
-                // Create fragment shader
-                let fragment_shader = gl.create_shader(glow::FRAGMENT_SHADER).unwrap();
-                gl.shader_source(fragment_shader, fragment_shader_source);
-                gl.compile_shader(fragment_shader);
-
-                if !gl.get_shader_compile_status(fragment_shader) {
-                    let error = gl.get_shader_info_log(fragment_shader);
-                    panic!("Fragment shader compilation failed: {}", error);
-                }
-
-                // Create shader program
-                let program = gl.create_program().unwrap();
-                gl.attach_shader(program, vertex_shader);
-                gl.attach_shader(program, fragment_shader);
-                gl.link_program(program);
-
-                if !gl.get_program_link_status(program) {
-                    let error = gl.get_program_info_log(program);
-                    panic!("Program linking failed: {}", error);
-                }
-
-                // Clean up shaders (they're now linked into the program)
-                gl.delete_shader(vertex_shader);
-                gl.delete_shader(fragment_shader);
-
-                // Create VAO
-                let vao = gl.create_vertex_array().unwrap();
-                gl.bind_vertex_array(Some(vao));
-
-                // Set viewport
-                let size = window.inner_size();
-                gl.viewport(0, 0, size.width as i32, size.height as i32);
-
-                (program, vao)
-            };
-
-            // Request initial redraw
             window.request_redraw();
-            
+
             self.window = Some(window);
             self.gl_context = Some(gl_context);
             self.gl_surface = Some(surface);
-            self.gl = Some(gl);
-            self.shader_program = Some(shader_program);
-            self.vao = Some(vao);
+            self.program = Some(program);
         }
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent
+    ) {
         match event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                if let (Some(gl), Some(surface), Some(context), Some(program), Some(vao)) = 
-                    (&self.gl, &self.gl_surface, &self.gl_context, &self.shader_program, &self.vao) {
-                    unsafe {
-                        // Clear the screen
-                        gl.clear_color(0.1, 0.2, 0.3, 1.0);
-                        gl.clear(glow::COLOR_BUFFER_BIT);
+                if
+                    let (Some(surface), Some(context), Some(program)) = (
+                        &self.gl_surface,
+                        &self.gl_context,
+                        &self.program,
+                    )
+                {
+                    program.render().expect("Failed to render triangle");
 
-                        // Use shader program and VAO
-                        gl.use_program(Some(*program));
-                        gl.bind_vertex_array(Some(*vao));
-
-                        // Draw the triangle
-                        gl.draw_arrays(glow::TRIANGLES, 0, 3);
-
-                        // Clean up
-                        gl.bind_vertex_array(None);
-                        gl.use_program(None);
-                    }
                     surface.swap_buffers(context).unwrap();
                 }
             }
@@ -169,16 +105,22 @@ impl ApplicationHandler for App {
     }
 }
 
+impl Drop for App {
+    fn drop(&mut self) {
+        if let Some(program) = &self.program {
+            program.cleanup();
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event_loop = EventLoop::new()?;
-    
+
     let mut app = App {
         window: None,
         gl_context: None,
         gl_surface: None,
-        gl: None,
-        shader_program: None,
-        vao: None,
+        program: None,
     };
 
     event_loop.run_app(&mut app)?;
