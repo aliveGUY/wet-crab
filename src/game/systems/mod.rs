bc2af4ad38078599556450e78ee5191ac005c215
illdub <illia.dubrovin@dreamhost.com>
2025-07-27 19:25:34 +0200

    whatever

--- src/game/systems/mod.rs
+++ src/game/systems/mod.rs
@@ -1,8 +1,6 @@
 pub mod render_system;
 pub mod movement_system;
-pub mod physics_system;
 
 // Re-export commonly used types
 pub use render_system::RenderSystem;
 pub use movement_system::{MovementSystem, CameraRotationSystem};
-pub use physics_system::PhysicsSystem;
